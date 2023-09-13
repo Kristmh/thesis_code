556f820510e33257a7e181581f24f1f3140d22c1
Kristian Marison Haugerud <haugerud.kristian@gmail.com>
2023-09-13 10:25:58 +0200

    feat: add all files

---
+++ script.sh
@@ -0,0 +1,7 @@
+module purge
+module load  Python/3.9.6-GCCcore-11.2.0
+module load  PyTorch/1.11.0-foss-2021a-CUDA-11.3.1
+module load  TensorFlow/2.6.0-foss-2021a-CUDA-11.3.1
+module load  JupyterLab/3.1.6-GCCcore-11.2.0
+tmux new -s jupyter_server
+
