e7a7d051fc6edae2c6d8b85bf04b36aa9e225ba7
Kristian Marison Haugerud <haugerud.kristian@gmail.com>
2023-11-13 09:47:23 +0100

    feat: high,med,low dataset pre for trainig form large data

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# mastes_thesis_code
-# thesis_code
+# Master thesis code 
+
+
+## Login
+ssh -L 7765:localhost:7765 -J username@gothmog.uio.no username@ml3.hpc.uio.no
+
+## Install conda 
+* module load Miniconda3/4.9.2
+* or install conda: https://docs.conda.io/projects/miniconda/en/latest/ 
+## Create env and install packages 
+conda create -n ml python=3.10
+* conda activate ml
+conda install jupyterlab
+conda install pandas
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+conda install --user transformers datasets accelerate scikit-learn papermill
+
+CUDA_VISIBLE_DEVICES=2 jupyter-lab --port=7765 --no-browser
+
+conda install -c conda-forge transformers datasets accelerate scikit-learn papermill
--- scripts_shared/preprocess_text.py
+++ scripts_shared/preprocess_text.py
@@ -5,15 +5,18 @@ import numpy as np
 from langdetect import detect
 
 def preprocess_text(text):
+
+    if not is_english(text):
+        # If no english set to NaN.
+        text = np.nan
+        return text
     text = text.lower()  # Lowercase text
     text = re.sub(f"[{re.escape(punctuation)}]", "", text)  # Remove punctuation
     text = " ".join(text.split())  # Remove extra spaces, tabs, and new lines
     text = re.sub(r"https?://\S+", "", text) # Remove URLs.
     text = emoji.demojize(text)
     text = re.sub(r':[a-z_]+:', '', text)  #removes the text representations of emojis
-    if not is_english(text):
-        # If no english set to NaN.
-        text = np.nan
+    text = re.sub(r'0x[a-fA-F0-9]+', '', text) # Removes hexadecimal strings (ref to commits)
     return text
 
 
