dddc96f58b4230940110df9caf87540daf361508
ec-krimhau <ec-krimhau@gpu-13.fox>
2024-03-29 07:36:42 +0100

    feat: prep done for training highest high vs rest jira

--- github_script.sh
+++ github_script.sh
@@ -1,3 +1,5 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
+/fp/homes01/u01/ec-krimhau/.local/bin/papermill 01_train_high_vs_med_low_top50_hp.ipynb 01_OUTPUT_train_high_vs_med_low_top50_hp.ipynb --log-level ERROR
+
